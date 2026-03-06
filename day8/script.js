5d8cac13bb8882fab7da2b3e20407f03e1ccd07e
vinay3254 <vinaygk219@gmail.com>
2026-03-06 22:40:03 +0530

    day8

---
+++ day8/script.js
@@ -0,0 +1,14 @@
+console.log("JavaScript connected!");
+
+let name = "vinay";
+let age ="21";
+
+console.log(typeof name);
+console.log(typeof age);
+
+
+let a = 10;
+let b = 5;
+
+console.log("sum:",a+b);
+console.log("multiply:",a*b);
